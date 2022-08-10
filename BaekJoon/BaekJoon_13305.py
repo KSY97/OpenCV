0ddcb573dbb0f99c4bce187602b553d5686171e6
KSY <ggomaDVLP@gmail.com>
2022-08-10 20:35:36 +0900

    220810

--- BaekJoon/BaekJoon_13305.py
+++ BaekJoon/BaekJoon_13305.py
@@ -11,14 +11,36 @@ add_val = 0
 all_val = 0
 
 # [2, 3, 1]
-# [5, 6, 7, 1]
+# [5, 2, 4, 1]
 
 for i in range(input_num-1):
-    if input_val[i] > input_val[i+1]:
-        add_dis += input_dis[i]
-        add_val = input_val[i] * add_dis
-        add_dis = 0
-    else:
-        add_dis += input_dis[i]
+    if val == 0:
+        val = input_val[i]
+
     if val > input_val[i]:
         val = input_val[i]
+
+    add_dis += input_dis[i]
+    add_val = val * add_dis
+    all_val += add_val
+    add_dis = 0
+    add_val = 0
+
+    # if input_val[i] > input_val[i+1]:
+    #     add_dis += input_dis[i]
+    #     add_val = val * add_dis
+    #     all_val += add_val
+    #     add_dis = 0
+    #     add_val = 0
+    #
+    #     print(all_val)
+    # else:
+    #     add_dis += input_dis[i]
+    #     add_val = val * add_dis
+    #     all_val += add_val
+    #     add_dis = 0
+    #     add_val = 0
+
+
+
+print(all_val)
